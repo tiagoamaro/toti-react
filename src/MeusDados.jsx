290800bad7bd7893e29ee7f45913c4a4a415a988
Tiago Amaro <tiagopadrela@gmail.com>
2020-12-19 09:47:47 -0300

    Criando component funcional que retorna dados sobre o aluno

---
+++ src/MeusDados.jsx
@@ -0,0 +1,9 @@
+export default function MeusDados() {
+    return (
+        <ul>
+            <li>Meu nome: nome aqui</li>
+            <li>Idade: minha idade</li>
+            <li>Nacionalidade: nacionalidade</li>
+        </ul>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MeusDados from './MeusDados';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MeusDados />
   </React.StrictMode>,
   document.getElementById('root')
 );
