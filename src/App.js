003a12b5a0f4971cbb5e5cfcd5761621e5e7c26d
Tiago Amaro <tiagopadrela@gmail.com>
2020-12-19 09:39:48 -0300

    Adicionando lista de tarefas no template principal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lista de tarefas</h1>
+      <ul>
+        <li>
+          <input type="checkbox" />
+          Batata
+        </li>
+        <li>
+          <input type="checkbox" />
+          Cenoura
+        </li>
+        <li>
+          <input type="checkbox" />
+          Maçãs
+        </li>
+      </ul>
     </div>
   );
 }
